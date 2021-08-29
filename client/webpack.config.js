6147fe650f429c74eee9abd2520509f980bb7d64
Huy Hoang Nguyen <hoangnh0099@gmail.com>
2021-08-30 03:18:19 +0700

    Update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,13 +4,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "main.js",
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
   ],
+  devServer: {
+    static: "./build",
+    hot: true,
+  },
 };
