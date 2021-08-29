a083fd45edc8e13cb901a7f8dc7fab385647ecc8
Huy Hoang Nguyen <hoangnh0099@gmail.com>
2021-08-30 02:46:24 +0700

    init project

---
+++ client/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
